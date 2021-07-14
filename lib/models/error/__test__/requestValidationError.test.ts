57fea26dac1f2bc8beccd27177225d77b9f96163
1onlinesolution <team@damianos.io>
2021-07-14 12:01:02 +0100

    express/errors

--- lib/models/error/__test__/requestValidationError.test.ts
+++ lib/models/error/__test__/requestValidationError.test.ts
@@ -5,7 +5,7 @@ describe('RequestValidationError', () => {
     const error = new RequestValidationError([]);
     expect(error instanceof Error).toBeTruthy();
     expect(error.name).toBe('RequestValidationError');
-    expect(error.message).toBe('Validation error(s) occurred');
+    expect(error.message).toBe('Invalid request parameters');
     expect(error.errors).not.toBeNull();
     expect(error.errors).toStrictEqual([]);
   });
